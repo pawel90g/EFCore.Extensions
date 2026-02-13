5c3fe37486bf16a90aadcdada70e8ee78d53effc
pawel90g <garbacik.pawel@gmail.com>
2026-02-13 15:40:48 +0100

    Support for Nullable objects

--- Garbacik.NetCore.EFCore.Extensions/DbContextExtensions.cs
+++ Garbacik.NetCore.EFCore.Extensions/DbContextExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -42,7 +43,7 @@ namespace Garbacik.NetCore.EFCore.Extensions
             typeof(T).GetProperties().ToList().ForEach(p =>
             {
                 if (!dataTable.Columns.Contains(p.Name))
-                    dataTable.Columns.Add(p.Name, p.PropertyType);
+                    dataTable.Columns.Add(p.Name, Nullable.GetUnderlyingType(p.PropertyType) ?? p.PropertyType);
             });
 
             dataTable.PrimaryKey = primaryKeys;
